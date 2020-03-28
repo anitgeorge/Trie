dcdd4b7ff832664619b81335b46be569b1eb6995
Anit George <anitgeorge89@gmail.com>
2020-03-28 20:03:33 +0530

    Adding comment

--- Trie/src/FileReader.java
+++ Trie/src/FileReader.java
@@ -5,7 +5,7 @@ import java.nio.file.Paths;
 import java.util.stream.Stream;
 
 public class FileReader {
-
+    //Known hard code dependency. Will fix later
     private static final Path filePath = Paths.get("c:/Users/anitg/IdeaProjects/Trie/resource", "words.txt");
 
     public void populateTrieFromFile(Trie trie) {
